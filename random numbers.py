7c565eeacd32f87efc120fa6f4e66b5b39fb477c
Lokesha P <105141493+lokesha3887@users.noreply.github.com>
2022-10-21 20:25:33 +0530

    Update random numbers.py

--- random numbers.py
+++ random numbers.py
@@ -3,30 +3,30 @@ import random
 help(random.random)
 
 # Generate 10 random number (float continuous)
-# for i in range(10):
-#     print(random.random())
-#
-# #Generate 10 uniform random number (integer continuous)
-# for i in range(10):
-#     print(random.uniform(5,10)) # which generates the number b/w 5 to 10
-#
-# ##Generate 10 uniform random number (integer discrete)
-# for i in range(50):
-#     print(random.randint(1,50))
+for i in range(10):
+    print(random.random())
+
+#Generate 10 uniform random number (integer continuous)
+for i in range(10):
+    print(random.uniform(5,10)) # which generates the number b/w 5 to 10
+
+##Generate 10 uniform random number (integer discrete)
+for i in range(50):
+    print(random.randint(1,50))
 
 # write program to generate random int number from 1 to 10 if random number is guess currecty stop the program(iteration)
-# import random
-# x=random.randint(1,10)
-# guess=int(input("Guess the random number"))
-# takes=1
-# while 1:
-#     takes+=1
-#     if guess==x:
-#         print("your guess is correct")
-#         break
-#     else:
-#         guess=int(input("Guess the random number")) # this for repeating the loop
-# print(f'total takes is {takes}')
+import random
+x=random.randint(1,10)
+guess=int(input("Guess the random number"))
+takes=1
+while 1:
+    takes+=1
+    if guess==x:
+        print("your guess is correct")
+        break
+    else:
+        guess=int(input("Guess the random number")) # this for repeating the loop
+print(f'total takes is {takes}')
 
 # ask person to number of trials if the 50% output of dice 1 and dice 2 sum>7  a persons wins
 trail=int(input(" Enter a number of trails: "))
