22c5e8a06efaa1405a5541d026fd14695868abd2
Lokesha P <105141493+lokesha3887@users.noreply.github.com>
2022-10-21 20:32:31 +0530

    Update type cating in python.py

--- type cating in python.py
+++ type cating in python.py
@@ -1,27 +1,27 @@
-# x=20
-# y=float(x)
-# print(type(x))
-# print(type(y))
-#
-# list=[2,5,7,8]
-# dict=tuple(list)
-# print(type(list))
-# print(type(dict))
-#
-# x=str(10)
-# print(x + 'hello')
-#
-# # task
-# n1=int(input('enter a num'))
-# n2=int(input('enter a num'))
-#
-# print("{} is {}% of {}".format(n1,n1%n2,n2))
+x=20
+y=float(x)
+print(type(x))
+print(type(y))
 
-# n1=float(input('enter a num'))
-# n2=int(n1)
-# print("integer part is {}".format(n2))
-# print("fractinal part is {}".format(n1-n2))
-#print(f"fractinal part is {n1-n2})
+list=[2,5,7,8]
+dict=tuple(list)
+print(type(list))
+print(type(dict))
+
+x=str(10)
+print(x + 'hello')
+
+# task
+n1=int(input('enter a num'))
+n2=int(input('enter a num'))
+
+print("{} is {}% of {}".format(n1,n1%n2,n2))
+
+n1=float(input('enter a num'))
+n2=int(n1)
+print("integer part is {}".format(n2))
+print("fractinal part is {}".format(n1-n2))
+print(f"fractinal part is {n1-n2})
 
 x=eval(str(input("enter 3 digits")))
 
